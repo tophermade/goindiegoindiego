1fa0acd40061d798046cd77c220a05d63aff7579
Topher Wilson <tophermade@gmail.com>
2016-07-06 02:19:31 -0600

    starting template routes

--- both/iron-routes.js
+++ both/iron-routes.js
@@ -4,7 +4,27 @@ Router.configure({
 });
 
 
-// primary routes
+// simple routes
 Router.route("/", function(){
-	this.render("dashTemplate.html");
+	this.render("dashTemplate");
+});
+
+
+// page routes
+Router.route("/pages/", function(){
+	this.render("pagesIndexTemplate");
+});
+
+Router.route("/pages/:id", function(){
+	this.render("pagesSingleTemplate");
+});
+
+
+// post routes
+Router.route("/posts/", function(){
+	this.render("postsIndexTemplate");
+});
+
+Router.route("/posts/:id", function(){
+	this.render("postsSingleTemplate");
 });
